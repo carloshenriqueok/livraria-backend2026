80152f8ec3a2fe663a79a90bee668d7758a36e84
carloshenriqueok <ifcarloshenrique@gmail.com>
2026-04-07 16:36:10 -0300

    FEAT: inclusão do uploads de imagens e documentos

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -21,6 +23,8 @@ from core.views import (
     UserViewSet,
 )
 
+from uploader.router import router as uploader_router
+
 router = DefaultRouter()
 
 router.register(r'autores', AutorViewSets, basename='autores')
@@ -31,6 +35,7 @@ router.register(r'usuarios', UserViewSet, basename='usuarios')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/media/', include(uploader_router.urls)),
     # OpenAPI 3
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path(
@@ -52,3 +57,5 @@ urlpatterns = [
     # API
     path('api/', include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
